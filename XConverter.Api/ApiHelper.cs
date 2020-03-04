9bc7a3533beec9c928630c3c30777170d253318b
Victor A <victorantos@users.noreply.github.com>
2020-03-04 14:58:57 +0000

    Add helper class

---
+++ XConverter.Api/ApiHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace XConverter.Api
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
