599c9f780b25f14a0fe0d4f1f52b6a916042096d
Victor A <victorantos@users.noreply.github.com>
2020-03-04 15:01:38 +0000

    Add views

---
+++ XConverter/Views/CurrencyView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using XConverter.Api;
+using XConverter.Api.Models;
+
+namespace XConverter.Views
+{
+    /// <summary>
+    /// Interaction logic for NewCurrencyView.xaml
+    /// </summary>
+    public partial class CurrencyView : UserControl
+    {
+        public CurrencyView()
+        {
+            InitializeComponent();
+        }
+
+        private CurrencyExchange currencyExchange;
+        private async Task LoadData()
+        {
+            //currencyExchange = await ExchangeRateProcessor.LoadData("GBP");
+        }
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            //await LoadData();
+
+        }
+ 
+    }
+}
---
+++ XConverter/Views/ShellView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using XConverter.Api;
+
+namespace XConverter.Views
+{
+    /// <summary>
+    /// Interaction logic for ShellView.xaml
+    /// </summary>
+    public partial class ShellView : Window
+    {
+
+        public ShellView()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+         
+        }
+    }
+}
