a366f86e9dd22933999811f715e49da164815784
Victor A <victorantos@users.noreply.github.com>
2020-03-04 14:59:14 +0000

    Add exchange rate processor and model

---
+++ XConverter.Api/ExchangeRateProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using XConverter.Api.Models;
+
+namespace XConverter.Api
+{
+    public class ExchangeRateProcessor
+    {
+        public static async Task<CurrencyExchange> LoadData(string baseRate)
+        {
+            string url = "https://api.exchangeratesapi.io/latest?base=" + baseRate;
+          
+            using(HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                    return await response.Content.ReadAsAsync<CurrencyExchange>();
+                else
+                    throw new Exception(response.ReasonPhrase);
+            }
+        }
+    }
+}
---
+++ XConverter.Api/Models/CurrencyExchange.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XConverter.Api.Models
+{
+    public class CurrencyExchange
+    {
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public Dictionary<string, double> Rates { get; set; }
+    }
+}
